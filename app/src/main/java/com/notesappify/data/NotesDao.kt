9b83cb6a7eb8bb73df24a929979f45387e25d3d2
ALT98 <andreslt09@hotmail.com>
2026-05-12 16:28:32 -0600

    [UPDATE] dao implementation

---
+++ app/src/main/java/com/notesappify/data/NotesDao.kt
@@ -0,0 +1,24 @@
+package com.notesappify.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NotesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Notes)
+
+    @Delete
+    suspend fun deleteNote(note: Notes)
+
+    @Update
+    suspend fun updateNote(note: Notes)
+
+    @Query("select * from notes")
+    fun getAllNotes(): Flow<List<Notes>>
+}
