dd2893384ce3e3db7a62735aeb0ecf43989ec3f6
ALT98 <andreslt09@hotmail.com>
2026-05-12 15:44:40 -0600

    [UPDATE] hilt implemented

--- app/src/main/java/com/notesappify/ui/viewmodel/NotesViewModel.kt
+++ app/src/main/java/com/notesappify/ui/viewmodel/NotesViewModel.kt
@@ -2,11 +2,14 @@ package com.notesappify.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.notesappify.data.Notes
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class NotesViewModel: ViewModel() {
+@HiltViewModel
+class NotesViewModel @Inject constructor() : ViewModel() {
 
     private val _notesValidation = MutableStateFlow(Notes())
     val notesValidation = _notesValidation.asStateFlow()
